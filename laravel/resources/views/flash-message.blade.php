3473c08db6664f5e4c5e9a885704a5d07447ae8d
xpardo <xpullodsmx@gmail.com>
2022-01-20 19:18:14 +0100

    flash

--- laravel/resources/views/flash-message.blade.php
+++ laravel/resources/views/flash-message.blade.php
@@ -7,3 +7,16 @@
         {{ $message }} <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>    
 </nav>
+
+
+
+<!--pie pagina-->
+<footer>
+
+<nav class="navbar fixed-bottom  justify-content-center" style="background-color: #b08fee;">
+    <p><a class="nav-link disabled" href="#" >2DAW 2021-2022</a></p>
+    <p><a class="nav-link disabled" href="https://agora.xtec.cat/iesjoaquimmir/">Institut joaquim mir</a></p>
+    <p><a class="nav-link disabled" >Xenia Pardo | Grup 08</a></p>
+</nav>
+
+</footer>
--- laravel/storage/framework/views/eb744dbeb6052fff942f93e7686881fbd06eef39.php
+++ laravel/storage/framework/views/eb744dbeb6052fff942f93e7686881fbd06eef39.php
@@ -130,16 +130,7 @@
                 </div>
             </div>
         </div>
-<!--pie pagina-->
-        <footer>
 
-            <nav class="navbar fixed-bottom  justify-content-center" style="background-color: #b08fee;">
-                <p><a class="nav-link disabled" href="#" >2DAW 2021-2022</a></p>
-                <p><a class="nav-link disabled" href="https://agora.xtec.cat/iesjoaquimmir/">Institut joaquim mir</a></p>
-                <p><a class="nav-link disabled" >Xenia Pardo | Grup 08</a></p>
-            </nav>
-
-        </footer>
     </body>
 </html>
 <?php /**PATH /var/www/html/tarda/2Daw07/laravel/resources/views/welcome.blade.php ENDPATH**/ ?>
